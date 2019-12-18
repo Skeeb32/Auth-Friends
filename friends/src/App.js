f32e042c5dbad777876a8c74af29ec510b8f8315
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-12-17 16:06:12 -0800

    Stand Up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router';
+import { Reset } from 'styled-reset';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
+import "../src/App.css"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <Reset />
+      <Route exact path ="/" component={Login} />
+      <PrivateRoute path="/friendsList" component={FriendsList} />
     </div>
   );
 }
